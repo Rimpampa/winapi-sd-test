6ccd49389b652172ca21a7fbdf0cf8826bd5fd2c
Riccardo Ripanti <riccardo.ripanti01@gmail.com>
2023-06-25 11:40:58 +0200

    Move `DevInterfaceData` to its own module

--- src/ifdata.rs
+++
@@ -1,6 +0,0 @@
-use crate::devset::DevSet;
-
-use winapi::um::setupapi::*;
-
-pub struct DevInterfaceData<'a>(&'a DevSet, SP_DEVICE_INTERFACE_DATA);
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(concat_idents)]
 #![feature(decl_macro)]
+#![feature(new_uninit)]
+#![feature(slice_ptr_get)]
 
+pub mod devdata;
 pub mod devprop;
 pub mod devset;
 pub mod win;
